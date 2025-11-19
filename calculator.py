ec4b732f15e7e39bf8319b3dea4f7ea83b1713f3
Ryan Buehler <ryanbuehler@ryan-buehlers-macbook.local>
2025-11-19 18:47:44 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,21 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-# First example
+import math
 def add(a, b): 
-    pass
+    return a + b
+def sub(a,b):
+    return a - b
+def mul(a,b):
+    return a * b
+def div(a,b):
+    if a == 0:
+        raise ZeroDivisionError("Division by zero is not allowed")
+    return b / a
+def log(a,b):
+    if a <= 0:
+        raise ValueError("Base must be positive and not equal to 1")
+    if b <= 0:
+        raise ValueError("Argument must be positive")
+    return math.log(b,a)
+def exp(a,b):
+    return a ** b
 
 
